005fc85bd60a15535a96ab32b4527f7ec1275c4f
rachnashah99 <rachna.shah@mnsu.edu>
2023-02-26 12:02:35 -0600

    New Code

--- shahsCafe.py
+++ shahsCafe.py
@@ -1,7 +1,7 @@
 
 
 
-limit = 50.00
+sum = 0.00
 bev= ["mocha", "Water", "fruit juice", "chailatte"]
 amt = [4.25, 1.00, 3.25, 3.00, 14.25, 16.75, 12.70, 11.80, 3.15, 4.90, 3.50]
 
@@ -12,20 +12,20 @@ bev_ch = int(input("Hello and welcome to the Shah's Cafe.\nWhat would you like t
 flag_drinks = False
 while flag_drinks == False:
   if bev_ch== 0:
-    limit = limit-(amt[0])
-    print("Thank you, {} sounds perfect! You have ${} remaining.\n".format(bev[0], limit))
+    sum = sum+(amt[0])
+    print("Thank you, {} sounds perfect! You have ${} remaining.\n".format(bev[0], sum))
     flag_drinks = True
   elif bev_ch== 1:
-    limit = limit-(amt[1])
-    print("Thank you, {} sounds perfect! You have ${} remaining.\n".format(bev[1], limit))
+    sum = sum+(amt[1])
+    print("Thank you, {} sounds perfect! You have ${} remaining.\n".format(bev[1], sum))
     flag_drinks = True
   elif bev_ch== 2:
-    limit = limit-(amt[2])
-    print("Thank you, {} sounds perfect! You have ${} remaining.\n".format(bev[2], limit))
+    sum = sum+(amt[2])
+    print("Thank you, {} sounds perfect! You have ${} remaining.\n".format(bev[2], sum))
     flag_drinks = True
   elif bev_ch== 3:
-    limit = limit-(amt[3])
-    print("Thank you, {} sounds perfect! You have ${} remaining.\n".format(bev[3], limit))
+    sum = sum+(amt[3])
+    print("Thank you, {} sounds perfect! You have ${} remaining.\n".format(bev[3], sum))
     flag_drinks = True
   else:
     bev_ch = int(input("Sorry, that doesn't seem to be an option. Try again:\n"))
@@ -36,20 +36,20 @@ main_ch = int(input("Great! Now, what can I get you for the main course?\n"
 flag_meal = False
 while flag_meal == False:
   if main_ch== 0:
-    limit = limit-(amt[4])
-    print("Yum, {} sounds good! You have ${} remaining.\n".format(main_course[0], limit))
+    sum = sum+(amt[4])
+    print("Yum, {} sounds good! You have ${} remaining.\n".format(main_course[0], sum))
     flag_meal = True
   elif main_ch == 1:
-    limit = limit-(amt[5])
-    print("Yum, {} sounds good! You have ${} remaining.\n".format(main_course[1], limit))
+    sum = sum+(amt[5])
+    print("Yum, {} sounds good! You have ${} remaining.\n".format(main_course[1], sum))
     flag_meal = True
   elif main_ch == 2:
-    limit = limit-(amt[6])
-    print("Yum, {} sounds good! You have ${} remaining.\n".format(main_course[2], limit))
+    sum = sum+(amt[6])
+    print("Yum, {} sounds good! You have ${} remaining.\n".format(main_course[2], sum))
     flag_meal = True
   elif main_ch == 3:
-    limit = limit-(amt[7])
-    print("Yum, {} sounds good! You have ${} remaining.\n".format(main_course[3], limit))
+    sum = sum+(amt[7])
+    print("Yum, {} sounds good! You have ${} remaining.\n".format(main_course[3], sum))
     flag_meal = True
   else:
     main_ch = int(input("Sorry, that doesn't seem to be an option. Try again:\n"))
@@ -60,23 +60,23 @@ dess_ch = int(input("And finally, what would you like for dessert?\n"
 flag_dess = False
 while flag_dess == False:
   if dess_ch == 0:
-    limit = limit-(amt[8])
-    print("Delicious {} coming right up! You have ${} remaining.\n".format(dess[0], limit))
+    sum = sum+(amt[8])
+    print("Delicious {} coming right up! You have ${} remaining.\n".format(dess[0], sum))
     flag_dess = True
   elif dess_ch == 1:
-    limit = limit-(amt[9])
-    print("Delicious {} coming right up! You have ${} remaining.\n".format(dess[1], limit))
+    sum = sum+(amt[9])
+    print("Delicious {} coming right up! You have ${} remaining.\n".format(dess[1], sum))
     flag_dess = True
   elif dess_ch == 2:
-    limit = limit-(amt[10])
-    print("Delicious {} coming right up! You have ${} remaining.\n".format(dess[2], limit))
+    sum = sum+(amt[10])
+    print("Delicious {} coming right up! You have ${} remaining.\n".format(dess[2], sum))
     flag_dess = True
   else:
     dess_ch = int(input("Sorry, that doesn't seem to be an option. Try again:\n"))
 print("{}.....${}\n{}.....${}\n{}.....${}\n".format(bev[bev_ch]
 , amt[bev_ch], main_course[main_ch], amt[main_ch+4], dess[dess_ch]
 , amt[dess_ch+ 8]))
-print("Total.....${}\n......................".format(50-limit))
+print("Total.....${}\n......................".format(sum))
 print("Thank you for visiting Shah's Cafe! See you again")
 
 
